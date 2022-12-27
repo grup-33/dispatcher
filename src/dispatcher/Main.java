83289b15cf0a1b765d859768a328ca45c697abb7
rumeysa <rbayrak677@gmail.com>
2022-12-27 19:44:38 +0300

    processList added

--- src/dispatcher/Main.java
+++ src/dispatcher/Main.java
@@ -1,36 +1,6 @@
 package dispatcher;
 
-import java.io.BufferedReader;
-import java.io.FileReader;
-import java.io.IOException;
-
 public class Main {
   public static void main(String[] args) {
-    BufferedReader reader = null;
-    try {
-      reader = new BufferedReader(new FileReader("giris.txt"));
-      String line;
-      while ((line = reader.readLine()) != null) {
-        String[] lineData = line.split(",");
-        String variable1 = lineData[0];
-        String variable2 = lineData[1];
-        String variable3 = lineData[1];
-       	System.out.print(variable1+" ");
-       	System.out.print(variable2+" ");
-       	System.out.println(variable3);
-       	System.out.println("      ");
-      }
-    } catch (IOException e) {
-      e.printStackTrace();
-    } finally {
-      if (reader != null) {
-        try {
- 
-          reader.close();
-        } catch (IOException e) {
-          e.printStackTrace();
-        }
-      }
-    }
   }
 }
---
+++ src/dispatcher/ProcessBuilder.java
@@ -0,0 +1,19 @@
+package dispatcher;
+import java.io.IOException;
+import java.util.List;
+import java.util.Scanner;
+
+public class ProcessBuilder {
+
+	public int varisZamani;
+	public int oncelik;
+	public int patlamaZamani;
+	
+	public int id;
+	public int durum;
+	
+	public Process process;
+	public ProcessBuilder() {
+		durum=0;
+	}
+}
